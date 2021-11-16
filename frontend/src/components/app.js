d151bd2c30b03bf35da2552da6e80f9eb9414d84
jayzizzle <dj.jonzamora@gmail.com>
2021-11-15 16:30:34 -0800

    clear default react-app

---
+++ frontend/src/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+// import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+export const App = () => {
+  <>
+    <div>
+      <h1>Mixtapes</h1>
+      <Routes></Routes>
+    </div>
+  </>;
+};
