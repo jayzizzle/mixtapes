e55b740ba6930908b244ccf190842917330529a4
jayzizzle <dj.jonzamora@gmail.com>
2021-11-15 18:20:23 -0800

    add register form

--- frontend/src/components/session/register.jsx
+++ frontend/src/components/session/register.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Enter Username'
+          autoComplete='username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          type='email'
+          placeholder='Enter Email'
+          autoComplete='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          type='password'
+          placeholder='Enter Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <input
+          type='password'
+          placeholder='Confirm Password'
+          value={password2}
+          onChange={(e) => setPassword2(e.target.value)}
+        />
+        <br />
+        <button>Register</button>
+      </form>
     </div>
   );
 };
