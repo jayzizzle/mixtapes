1db3dbac5d1f8bd575046fdfdaa25c29efcbdfe3
jayzizzle <dj.jonzamora@gmail.com>
2021-11-17 18:05:02 -0800

    test auth routes

--- frontend/src/util/route_util.js
+++ frontend/src/util/route_util.js
@@ -2,43 +2,37 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Navigate, useNavigate } from 'react-router-dom';
 
-export const withRouter = (Component) => {
-  const Wrapper = (props) => {
-    const history = useNavigate();
-
-    return <Component history={history} {...props} />;
-  };
-
-  return Wrapper;
+const RequireAuth = ({ children, loggedIn }) => {
+  return loggedIn ? children : <Navigate to='/login' replace />;
 };
 
-const Auth = ({ component: Component, path, loggedIn, exact }) => (
-  <Route
-    path={path}
-    exact={exact}
-    render={(props) =>
-      !loggedIn ? (
-        <Component {...props} />
-      ) : (
-        <Navigate replace to='/dashboard' />
-      )
-    }
-  />
-);
+// const Auth = ({ component: Component, path, loggedIn, exact }) => (
+//   <Route
+//     path={path}
+//     exact={exact}
+//     render={(props) =>
+//       !loggedIn ? (
+//         <Component {...props} />
+//       ) : (
+//         <Navigate replace to='/dashboard' />
+//       )
+//     }
+//   />
+// );
 
-const Protected = ({ component: Component, loggedIn, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      loggedIn ? <Component {...props} /> : <Navigate replace to='/login' />
-    }
-  />
-);
+// const Protected = ({ component: Component, loggedIn, ...rest }) => (
+//   <Route
+//     {...rest}
+//     render={(props) =>
+//       loggedIn ? <Component {...props} /> : <Navigate replace to='/login' />
+//     }
+//   />
+// );
 
 const mapStateToProps = (state) => ({
   loggedIn: state.session.isAuthenticated,
 });
 
-export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
+export const AuthRoute = connect(mapStateToProps)(RequireAuth);
 
-export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
+// export const ProtectedRoute = connect(mapStateToProps)(Protected);
