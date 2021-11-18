1d83264173f809d0480594b92f1f0e2e2d0eaf3d
jayzizzle <dj.jonzamora@gmail.com>
2021-11-17 18:23:12 -0800

    add logout button

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export const Dashboard = () => {
+export const Dashboard = (props) => {
   return (
     <div>
       <h1>Dashboard</h1>
+      <button onClick={() => props.logout()}>Log Out</button>
     </div>
   );
 };
