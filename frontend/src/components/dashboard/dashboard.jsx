10d5a34d33ef295eefdb04e6e2fd27e7b4837030
jayzizzle <dj.jonzamora@gmail.com>
2021-11-18 16:14:05 -0800

    add personalized greeting

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 export const Dashboard = (props) => {
+  console.log(props.currentUser);
+  const { username } = props.currentUser;
   return (
     <div>
       <h1>Dashboard</h1>
+      <p>
+        Welcome back, <span className='capitalize italic'>{username}</span>!
+      </p>
       <button onClick={() => props.logout()}>Log Out</button>
     </div>
   );
