de10bcf6afde1f4c6d60c7df8765a03e8fd152ff
jayzizzle <dj.jonzamora@gmail.com>
2021-11-21 14:10:27 -0800

    add search functionality

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Dashboard = (props) => {
-  console.log(props.currentUser);
   const { username } = props.currentUser;
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let items = [];
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchQuery}`)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (result) {
+        items = result.items;
+        console.log(items);
+      });
+  };
+
   return (
     <div>
       <h1>Dashboard</h1>
       <p>
         Welcome back, <span className='capitalize italic'>{username}</span>!
       </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Search'
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <br />
+        <button>Search</button>
+      </form>
       <button onClick={() => props.logout()}>Log Out</button>
     </div>
   );
