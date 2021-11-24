18f7fe5b29454f6571dde4023882fe5c97baaae2
jayzizzle <dj.jonzamora@gmail.com>
2021-11-23 16:06:43 -0800

    testing multiple results

--- frontend/src/components/books/add_book.jsx
+++ frontend/src/components/books/add_book.jsx
@@ -2,25 +2,47 @@ import React, { useState } from 'react';
 
 export const AddBook = () => {
   // const { username } = props.currentUser;
+  const [resultArray, setResultArray] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
-  const [thumbnail, setThumbnail] = useState('');
-  const [title, setTitle] = useState('');
+  // const [thumbnail, setThumbnail] = useState('');
+  // const [title, setTitle] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (thumbnail) setThumbnail('');
+    // if (thumbnail) setThumbnail('');
     if (searchQuery) {
       fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchQuery}`)
         .then((res) => res.json())
         .then((result) => {
-          console.log(result.items[0]);
-          if (result.items[0].volumeInfo.imageLinks) {
-            setThumbnail(result.items[0].volumeInfo.imageLinks.smallThumbnail);
-          }
-          setTitle(result.items[0].volumeInfo.title);
+          setResultArray(result.items);
+          console.log(result.items);
+          // console.log(result.items[0]);
+          // if (result.items[0].volumeInfo.imageLinks) {
+          //   setThumbnail(result.items[0].volumeInfo.imageLinks.smallThumbnail);
+          // }
+          // setTitle(result.items[0].volumeInfo.title);
         });
     } else {
       console.log('Search field cannot be empty.');
+    }
+  };
+
+  const resultList = (resultArray) => {
+    if (resultArray) {
+      resultArray.map((book) => (
+        <li>
+          <img
+            src={
+              book.volumeInfo.imageLinks
+                ? book.volumeInfo.imageLinks.smallThumbnail
+                : null
+            }
+            alt='thumbnail'
+          />
+          <br />
+          <h4>{book.volumeInfo.title}</h4>
+        </li>
+      ));
     }
   };
 
@@ -37,10 +59,12 @@ export const AddBook = () => {
         <br />
         <button>Search</button>
       </form>
-      {thumbnail ? <img src={thumbnail} alt='thumbnail' /> : null}
+      <br />
+      <ul>{resultList(resultArray)}</ul>
+      {/* {thumbnail ? <img src={thumbnail} alt='thumbnail' /> : null}
       <br />
       {title ? <h3>{title}</h3> : null}
-      <br />
+      <br /> */}
     </div>
   );
 };
