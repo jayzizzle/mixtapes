24d709a1d3d632fbc6aed72010767764dff76096
jayzizzle <dj.jonzamora@gmail.com>
2021-11-24 16:40:08 -0800

    add items to header

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
 export const Header = (props) => {
-  const { loggedIn } = props;
+  const { loggedIn, currentUser } = props;
+  const menuItems = ['My Bookshelf', 'Wish List', 'Recommendation List'];
+
   if (!loggedIn) {
     return null;
   } else {
     return (
       <header>
-        <p>This is the left header.</p>
-        <p>This is the right header.</p>
+        <div>
+          Welcome back,&nbsp;
+          <span className='capitalize italic'>{currentUser.username}</span>!
+        </div>
+        <div>
+          <ul>
+            {menuItems.map((menuItem, i) => (
+              <li key={i}>{menuItem}</li>
+            ))}
+          </ul>
+        </div>
       </header>
     );
   }
