d523189cf98da8db1ad8c138db7ffc83c5dbdfdb
jayzizzle <dj.jonzamora@gmail.com>
2021-11-24 19:38:01 -0800

    update header details

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const Header = (props) => {
   const { loggedIn, currentUser } = props;
-  const menuItems = ['My Bookshelf', 'Wish List', 'Recommendations'];
+  const menuItems = [
+    { title: 'My Bookshelf', url: '/bookshelf' },
+    { title: 'Wish List', url: '/wishlist' },
+    { title: 'Recommendations', url: '/recommendations' },
+  ];
 
   if (!loggedIn) {
     return null;
@@ -14,9 +19,11 @@ export const Header = (props) => {
           <span className='capitalize italic'>{currentUser.username}</span>!
         </div>
         <div>
-          <ul className='flex j-evenly a-center'>
+          <ul className='flex j-evenly a-center header-links'>
             {menuItems.map((menuItem, i) => (
-              <li key={i}>{menuItem}</li>
+              <li key={i}>
+                <NavLink to={menuItem.url}>{menuItem.title}</NavLink>
+              </li>
             ))}
           </ul>
         </div>
