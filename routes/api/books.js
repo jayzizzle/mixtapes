c55464eb756d7bb0a3084f37558ca3e2be149c80
jayzizzle <dj.jonzamora@gmail.com>
2022-01-04 17:14:20 -0800

    add router books get bookshelf method

--- routes/api/books.js
+++ routes/api/books.js
@@ -5,6 +5,16 @@ const passport = require('passport');
 const keys = require('../../config/keys');
 const User = require('../../models/User');
 
+router.get(
+  '/:userId/bookshelf',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    User.findById({ _id: req.params.userId }).then((user) =>
+      res.json(user.books)
+    );
+  }
+);
+
 router.patch(
   '/:userId/bookshelf/addbook',
   passport.authenticate('jwt', { session: false }),
