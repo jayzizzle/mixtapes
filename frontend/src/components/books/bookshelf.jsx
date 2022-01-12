f831c2e4bbf29b283dfd760c53bd2cc5238dead2
jayzizzle <dj.jonzamora@gmail.com>
2022-01-11 17:38:38 -0800

    add current read status on bookshelf

--- frontend/src/components/books/bookshelf.jsx
+++ frontend/src/components/books/bookshelf.jsx
@@ -12,14 +12,17 @@ export const Bookshelf = (props) => {
   return (
     <div>
       <h3>My Bookshelf</h3>
-      <p>These are my books.</p>
       <ul>
         {props.bookshelf.map((book, i) => (
           <li key={i}>
             <a href={book.previewLink} target='_blank' rel='noreferrer'>
               <img src={book.thumbnail} alt={book.title} />
             </a>
+            <br />
             {`${book.title} - ${book.authors}`}
+            <br />
+            {book.isDone ? 'Completed' : 'Currently Unread'}
+            <br />
           </li>
         ))}
       </ul>
